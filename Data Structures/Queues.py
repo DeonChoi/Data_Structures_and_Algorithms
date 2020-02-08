d1c6613c4cb951750c422e4e08ae650698d7f3b2
Deon Choi <deon.choi@gmail.com>
2020-02-08 14:53:45 -0800

    Queues.py

--- Data Structures/Queues.py
+++ Data Structures/Queues.py
@@ -1,12 +1,26 @@
-class Queue(object):
-    def __init__(self, head=None):
-        self.storage = [head]
+class Queue:
+    def __init__(self):
+        self.items = []
 
     def enqueue(self, new_element):
-        self.storage.append(new_element)
+        self.items.append(new_element)
 
     def peek(self):
-        return self.storage[0]
+        return self.items[0]
 
     def dequeue(self):
-        return self.storage.pop(0)
+        return self.items.pop(0)
+
+    def isEmpty(self):
+        return self.items == []
+
+    def size(self):
+        return len(self.items)
+
+
+q=Queue()	
+q.enqueue(4)
+q.enqueue('dog')
+q.enqueue(True)
+print(q.size())
+print(q.isEmpty())
